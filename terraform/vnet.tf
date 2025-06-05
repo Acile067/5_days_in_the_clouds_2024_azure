500b31d6fddd154032596eb046599232aa1c3089
Aleksandar <acile067@gmail.com>
2025-06-05 10:32:09 +0200

    fix

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -52,3 +52,7 @@ resource azurerm_subnet snet_app_gateway {
   virtual_network_name              = azurerm_virtual_network.main.name
   address_prefixes                  = ["10.0.2.0/24"]
 }
+resource azurerm_subnet_network_security_group_association snet_app_gateway_nsg_association {
+  subnet_id                 = azurerm_subnet.snet_app_gateway.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
