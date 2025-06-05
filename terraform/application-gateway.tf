34c63a223a984af27d05d696be8d68ee35f248cc
Aleksandar <acile067@gmail.com>
2025-06-05 09:38:49 +0200

    fqdns

--- terraform/application-gateway.tf
+++ terraform/application-gateway.tf
@@ -44,6 +44,7 @@ resource azurerm_application_gateway main {
 
   backend_address_pool {
     name = local.backend_address_pool_name
+    fqdns = ["${azurerm_linux_web_app.backend.name}.azurewebsites.net"]
   }
 
   backend_http_settings {
